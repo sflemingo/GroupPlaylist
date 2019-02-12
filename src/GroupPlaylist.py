4ad285e52afabaa176e1eb8767e1af82ee092594
sflemingo <spencer@fleming.net>
2019-02-11 20:52:42 -0500

    fixed small indentation issue

--- src/GroupPlaylist.py
+++ src/GroupPlaylist.py
@@ -3,10 +3,15 @@ Author: Spencer Fleming
 File Name: GroupPlaylist.py
 Description: Interacts with client to retrieve artists and create a playlist
 """
+import spotipy_tools.SpotipyAuth as auth
 
-# not yet implemented
+SPOTIFY_USERNAME = 'spotify-username' # replace with your spotify username
+SPOTIFY_CLIENT_ID = 'spotify-client-id' # replace with your spotify client id
+SPOTIFY_CLIENT_SECRET = 'spotify-client-secret' # replace with your spotify client secret id
+SPOTIFY_REDIRECT_URI = 'spotify-redirect-uri' # replace with your spotify redirect uri
 
 def main():
+	spotify = auth.authorize_spotify_acct(SPOTIFY_USERNAME, SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET, SPOTIFY_REDIRECT_URI)
 
 if __name__ == '__main__':
     main()
--- src/spotipy_tools/SpotipyAuth.py
+++ src/spotipy_tools/SpotipyAuth.py
@@ -6,12 +6,15 @@ Description: Authorizes the Spotify account with spotipy
 import spotipy
 import spotipy.util as util
 
-def authorize_spotify_acct(username, client_id, client_secret, redirect_uri)
+def authorize_spotify_acct(username, client_id, client_secret, redirect_uri):
+	"""
+	authorizes the spotify account
+	returns the spotify object
+	"""
 	scope = 'playlist-modify-public'
 	token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
-
 	if token:
 		spotify = spotipy.Spotify(auth=token)
 		return spotify		
 	else:
-    	sys.exit("Can't retrieve token")
+		sys.exit("Can't retrieve token")
